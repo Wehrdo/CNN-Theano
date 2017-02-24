71412b44081392f1e1f560b2c292d64001bbeebd
David Wehr <dawehr@iastate.edu>
2017-02-23 23:39:45 -0600

    Kind of works for convolution, but runs out of memory

--- conv.py
+++ conv.py
@@ -7,31 +7,37 @@ from theano import shared
 import matplotlib.pyplot as plt
 import matplotlib.gridspec
 import matplotlib.colors
+import sys
 import math
 import time
 import pickle
 import itertools
+import im2col
+
+sys.setrecursionlimit(2000)
 
 MARGIN = 1
 
 class MLP:
-    def __init__(self, n_filters, layer_sizes, dropout_rate, trained_model=None):
-        kernel_size = 3
-        padding = 1
-
-        self.kernel_w = []
-        self.kernel_b = []
-
-
+    def __init__(self, layer_sizes, dropout_rate, trained_model=None):
         n_layers = len(layer_sizes) - 1
-
+        kernel_sizes = []
         if trained_model is None:
             self.weights = []
             self.biases = []
             # Initialize random values for weight and biase matrices
             for i in range(1, n_layers + 1):
-                w = np.random.standard_normal((layer_sizes[i], layer_sizes[i-1])) * math.sqrt(2 / layer_sizes[i-1])
-                b = np.random.standard_normal((layer_sizes[i], 1)) * math.sqrt(2 / layer_sizes[i-1])
+                n_kernels = layer_sizes[i][2]
+                kernel_size = layer_sizes[i-1][0] - layer_sizes[i][0] + 1
+                assert(kernel_size == layer_sizes[i-1][1] - layer_sizes[i][1] + 1) # Check that x/y dims match
+                w_mat_size = (n_kernels, layer_sizes[i-1][2] * kernel_size**2)
+                kernel_sizes.append(kernel_size)
+
+                w = np.random.standard_normal(w_mat_size) * math.sqrt(2 / w_mat_size[1])
+                b = np.random.standard_normal((n_kernels,1,1)) * math.sqrt(2 / w_mat_size[1])
+
+                # w = np.random.standard_normal((layer_sizes[i], layer_sizes[i-1])) * math.sqrt(2 / layer_sizes[i-1])
+                # b = np.random.standard_normal((layer_sizes[i], 1)) * math.sqrt(2 / layer_sizes[i-1])
                 self.weights.append(w)
                 self.biases.append(b)
         else:
@@ -42,62 +48,87 @@ class MLP:
 
         # Turn weights and biases into shared variables to be used in training and predicting
         self.weights = [shared(w, f"w{i}") for i, w in enumerate(self.weights)]
-        self.biases = [shared(b, f"b{i}", broadcastable=(False,True)) for i, b in enumerate(self.biases)]
+        self.biases = [shared(b, f"b{i}", broadcastable=(False,True,True)) for i, b in enumerate(self.biases)]
 
         # Variable for y value (correct values when training)
         y = T.dmatrix('y')
 
         # List of matrices for each layer's activations, plus one for input
-        activations = T.dmatrices(n_layers + 1)
+        activations = T.dtensor4s(n_layers)
+        activations.append(T.dtensor3())
+
+        images = T.tensor4('images')
+        activations[0] = images #im2col.im2col(images, kernel_sizes[0])
+
         # Dropout random number matrices
-        dropout_masks = []
-        np.random.seed(1)
-        np_rng = np.random.RandomState()
-        for i in range(1, n_layers):
-            srng = RandomStreams(np_rng.randint(1000000))
-            rv = srng.uniform((layer_sizes[i], y.shape[1]))
-            dropout_masks.append(rv)
+        # dropout_masks = []
+        # np.random.seed(1)
+        # np_rng = np.random.RandomState()
+        # for i in range(1, n_layers):
+        #     srng = RandomStreams(np_rng.randint(1000000))
+        #     rv = srng.uniform((layer_sizes[i][2], layer_sizes[i][0]*layer_sizes[i][0], y.shape[3]))
+        #     dropout_masks.append(rv)
         for l_i in range(n_layers):
             # Define layer function: max(0, w*x + b)
+            columnized = im2col.im2col(activations[l_i], kernel_sizes[l_i])
             if l_i == n_layers - 1: # Output layer
-                activations[l_i + 1] = self.biases[l_i] + T.dot(self.weights[l_i], activations[l_i])
+                activations[l_i + 1] = self.biases[l_i] + T.tensordot(self.weights[l_i], columnized, axes=1)
+            else:
+                activation = T.nnet.relu(self.biases[l_i] + T.tensordot(self.weights[l_i], columnized, axes=1))
+                activations[l_i + 1] = im2col.col2im(activation, layer_sizes[l_i + 1])
             # elif l_i == 0:
             #     activations[l_i + 1] = T.nnet.relu(self.biases[l_i] + T.dot(self.weights[l_i], activations[l_i]))
-            else:
-                pre_mask = T.nnet.relu(self.biases[l_i] + T.dot(self.weights[l_i], activations[l_i]))
-                dropout_mask = (dropout_masks[l_i] < dropout_rate) / dropout_rate
-                activations[l_i + 1] = pre_mask * dropout_mask
+            # else:
+            #     pre_mask = T.nnet.relu(self.biases[l_i] + T.dot(self.weights[l_i], activations[l_i]))
+            #     dropout_mask = (dropout_masks[l_i] < dropout_rate) / dropout_rate
+            #     activations[l_i + 1] = pre_mask * dropout_mask
 
+        # reshaped_output = im2col.col2im(activations[-1], (layer_sizes[-1][0], layer_sizes[-1][1]), layer_sizes[-1][2])
+        #
+        # ## Hinge loss
+        # # Indices of the correct classes
+        # correct_classes = T.argmax(y[0,0], axis=0, keepdims=True)
+        # # Actual values predicted for the correct classes
+        # correct_vals = reshaped_output[0,0,correct_classes, T.arange(y.shape[3])]
+        # # margin: max(0, predicted - correct_value + 1)
+        # margin_mat = T.maximum(0, reshaped_output[0,0] - T.repeat(correct_vals, repeats=y.shape[2], axis=0) + 1)
+        # # Loss for each training sample
+        # individual_losses = T.sum(margin_mat, axis=0) - margin_mat[correct_classes, T.arange(y.shape[3])]
+        # tot_loss = T.sum(individual_losses)
+        # # Average loss
+        # loss = tot_loss / y.shape[3]
 
-        ## Hinge loss
-        # Indices of the correct classes
-        correct_classes = T.argmax(y, axis=0, keepdims=True)
-        # Actual values predicted for the correct classes
-        correct_vals = activations[-1][correct_classes, T.arange(y.shape[1])]
-        # margin: max(0, predicted - correct_value + 1)
-        margin_mat = T.maximum(0, activations[-1] - T.repeat(correct_vals, repeats=y.shape[0], axis=0) + 1)
-        # Loss for each training sample
-        individual_losses = T.sum(margin_mat, axis=0) - margin_mat[correct_classes, T.arange(y.shape[1])]
-        tot_loss = T.sum(individual_losses)
-        # Average loss
-        loss = tot_loss / y.shape[1]
+        # TODO: Update loss to work with imagized version
+        n_classes = y.shape[0]
+        n_samples = y.shape[1]
+        reshaped_y = y.dimshuffle(1,0)
+        # activations[-1] is (C,1,N). Drop the middle dimension, then transpose
+        reshaped_x = activations[-1].reshape((n_classes, n_samples)).dimshuffle(1,0)
+        individual_losses = T.nnet.categorical_crossentropy(T.nnet.softmax(reshaped_x), reshaped_y)
+        loss = T.sum(individual_losses) / n_samples
 
         # List of matrices for each layer's activations, plus one for input
-        pred_activations = T.dmatrices(n_layers + 1)
+        pred_activations = T.tensor4s(n_layers)
+        pred_activations.append(T.tensor3())
+        pred_images = T.tensor4('pred_images')
+        pred_activations[0] = pred_images #im2col.im2col(pred_images, kernel_sizes[0])
         for l_i in range(n_layers):
+            columnized = im2col.im2col(pred_activations[l_i], kernel_sizes[l_i])
             # Define layer function: max(0, w*x + b)
             if l_i == n_layers - 1: # Output layer
-                pred_activations[l_i + 1] = self.biases[l_i] + T.dot(self.weights[l_i], pred_activations[l_i])
+                pred_activations[l_i + 1] = self.biases[l_i] + T.tensordot(self.weights[l_i], columnized, axes=1)
             else:
-                pred_activations[l_i + 1] = T.nnet.relu(self.biases[l_i] + T.dot(self.weights[l_i], pred_activations[l_i]))
-
+                activation = T.nnet.relu(self.biases[l_i] + T.tensordot(self.weights[l_i], columnized, axes=1))
+                pred_activations[l_i + 1] = im2col.col2im(activation, layer_sizes[l_i + 1])
+        pred_output = pred_activations[-1]
         # Create Theano function for predicting
-        self.predict = Tfunc([pred_activations[0]], pred_activations[-1])
+        self.predict = Tfunc([pred_images], pred_output)
 
         # List of expressions for derivatives: d_w1, d_w2, ... d_b1, d_b2,...
         derivatives = T.grad(loss, self.weights + self.biases)
         # Learning rate
         rate = T.dscalar('r')
+
         # How to update weights and biases when training
         B1 = 0.9
         B2 = 0.999
@@ -125,18 +156,18 @@ class MLP:
 
         # the_updates = [(var, var - rate*d_var) for var, d_var in zip(self.weights + self.biases, derivatives)]
         # Function for actually executing training
-        self.update_step = Tfunc([activations[0], y, rate], loss, updates=update_rules)
+        self.update_step = Tfunc([images, y, rate], loss, updates=update_rules, mode='DebugMode')
 
     def train(self, x, y, test_x, test_y, rate=0.002, batch_size=200, epochs=30):
         losses = []
         train_accuracies = []
         test_accuracies = []
         for epoch in range(epochs):
-            n_iters = int(x.shape[1] / batch_size)
+            n_iters = int(x.shape[3] / batch_size)
             for iter in range(n_iters):
-                selection = np.random.randint(0, x.shape[1], batch_size)
+                selection = np.random.randint(0, x.shape[3], batch_size)
                 # loss = self.update_weights(x[:,selection], y[:,selection], rate)
-                loss = self.update_step(x[:,selection], y[:,selection], rate)
+                loss = self.update_step(x[:,:,:,selection], y[:,selection], rate)
                 losses.append(loss)
             train_accuracies.append(calc_accuracy(self, x, y))
             test_accuracies.append(calc_accuracy(self, test_x, test_y))
@@ -238,7 +269,7 @@ def preprocess(data, params=None):
 
 def calc_accuracy(mlp, data_x, data_y):
     output = mlp.predict(data_x)
-    predictions = np.argmax(output, axis=0)
+    predictions = np.argmax(output[0,0], axis=0)
     accuracy = np.sum(np.argmax(data_y, axis=0) == predictions) / data_y.shape[1]
     return accuracy
 
@@ -267,6 +298,10 @@ def show_predictions(data_x, data_predictions, class_names={}, n_to_show=50):
             ax.axis('off')
     plt.show()
 
+def cifar_to_im(dataset):
+    n_images = dataset.shape[0]
+    return np.transpose(dataset.T.reshape((32,32,3,n_images), order='F'), [1,0,2,3]).astype('float64')
+
 if __name__ == '__main__':
     # with open('datasets/mnist.pkl3', 'rb') as data_f:
     with open('datasets/cifar-10-python/combined_data.pkl3', 'rb') as data_f:
@@ -274,32 +309,32 @@ if __name__ == '__main__':
     # with gzip.open('datasets/mnist.pkl3.gz', 'wb') as data_f:
     #     pickle.dump((train_set, test_set, validation_set), data_f)
 
-    amt = 50000
+    amt = 5000
     train_data = train_set[0][0:amt]
     train_labels = train_set[1][0:amt]
 
     transform = preprocess(train_data)
 
-    train_x = train_data.T
+    train_x = cifar_to_im(train_data)
+
     # n_classes = len(np.unique(train_labels))
     train_y = transform_labels(train_labels, 10)
 
     test_y = transform_labels(test_set[1], 10)
     test_data = test_set[0]
     preprocess(test_data, transform)
-    test_x = test_data.T
+    test_x = cifar_to_im(test_data)
     # adam, RMSprop
     train_scratch = True
     n_hidden = 100
-    layer_sizes = [train_x.shape[0], 100,  10]
+    layer_sizes = [(32,32,3), (30,30,10), (1,1,10)]
     dropout = 0.7
-    batch = 200
-    epochs = 15
+    batch = 50
+    epochs = 5
     alpha = 0.001
 
     if train_scratch:
         mlp = MLP(layer_sizes, dropout)
-
         start_time = time.time()
         losses, train_accuracies, test_accuracies = mlp.train(train_x, train_y, test_x, test_y, alpha, batch, epochs)
         print("Took " + str(time.time() - start_time) + " seconds to train")
@@ -310,11 +345,11 @@ if __name__ == '__main__':
         plt.plot(test_accuracies, ls="none", marker='o')
         plt.show()
 
-        with open('trained_model_cifar.pkl', 'wb') as file:
+        with open('trained_conv_cifar.pkl', 'wb') as file:
             mlp_data = ([w.get_value() for w in mlp.weights], [b.get_value() for b in mlp.biases])
             pickle.dump((mlp_data, train_accuracies, test_accuracies, losses), file)
     else:
-        with open('trained_model_cifar.pkl', 'rb') as file:
+        with open('trained_conv_cifar.pkl', 'rb') as file:
             pre_trained, train_accuracies, test_accuracies, losses = saved_data = pickle.load(file)
             mlp = MLP(layer_sizes, dropout, pre_trained)
 
--- im2col.py
+++ im2col.py
@@ -1,51 +1,81 @@
 import numpy as np
+import theano
 import theano.tensor as T
 from theano import function as Tfunc
 
+# def im2col(img, kernel_size):
+#     depth = img.shape[2]
+#     # Stack along depth
+#     duplicated = T.tile(img, (1, 1, kernel_size**2, 1))
+#     # Roll each repeated layer so each hole through (x,y) has all values from the filter block
+#     for c in range(kernel_size):
+#         layer = c*kernel_size*depth
+#         next_layer = layer + depth*kernel_size
+#         # Roll all remaining layers along x-direction
+#         duplicated = T.set_subtensor(duplicated[:,:,layer:next_layer,:], T.roll(duplicated[:,:,layer:next_layer,:], -c, axis=1))
+#         for r in range(kernel_size):
+#             layer = c*kernel_size*depth + r*depth
+#             next_layer = layer + depth
+#             # Roll single layer along y-direction
+#             duplicated = T.set_subtensor(duplicated[:,:,layer:next_layer,:], T.roll(duplicated[:,:,layer:next_layer,:], -r, axis=0))
+#
+#     convolved_dims = (img.shape[0] - kernel_size + 1, img.shape[1] - kernel_size + 1)
+#     duplicated = duplicated.dimshuffle((3, 1, 0, 2))
+#     flattened = T.reshape(duplicated[:,:convolved_dims[0],:convolved_dims[1],:],
+#                           (img.shape[3], convolved_dims[0]*convolved_dims[1], depth*(kernel_size**2)))
+#     return flattened.dimshuffle((2, 1, 0))
+
 def im2col(img, kernel_size):
     depth = img.shape[2]
-    # Stack along depth
-    duplicated = T.tile(img, (1, 1, kernel_size**2, 1))
-    # Roll each repeated layer so each hole through (x,y) has all values from the filter block
-    for c in range(kernel_size):
-        layer = c*kernel_size*depth
-        next_layer = layer + depth*kernel_size
-        # Roll all remaining layers along x-direction
-        duplicated = T.set_subtensor(duplicated[:,:,layer:next_layer,:], T.roll(duplicated[:,:,layer:next_layer,:], -c, axis=1))
-        for r in range(kernel_size):
-            layer = c*kernel_size*depth + r*depth
-            next_layer = layer + depth
-            # Roll single layer along y-direction
-            duplicated = T.set_subtensor(duplicated[:,:,layer:next_layer,:], T.roll(duplicated[:,:,layer:next_layer,:], -r, axis=0))
-
     convolved_dims = (img.shape[0] - kernel_size + 1, img.shape[1] - kernel_size + 1)
-    duplicated = duplicated.dimshuffle((3, 1, 0, 2))
-    flattened = T.reshape(duplicated[:,:convolved_dims[0],:convolved_dims[1],:],
-                          (img.shape[3], convolved_dims[0]*convolved_dims[1], depth*(kernel_size**2)))
-    return flattened.dimshuffle((2, 1, 0))
+    out_height = depth * (kernel_size**2)
+    n_images = img.shape[3]
 
-def col2im(img, dims, kernel_size, depth):
+    def set_column(out_idx, columnized, orig_img):
+        col = T.cast(out_idx / convolved_dims[0], 'int32')
+        row = out_idx - convolved_dims[0]*col
+        vals = orig_img[row:row+kernel_size,col:col+kernel_size,:,:]
+        shaped = T.reshape(vals, (out_height,n_images))
+        return T.set_subtensor(columnized[:,out_idx,:], shaped)
+
+    n_positions = convolved_dims[0]*convolved_dims[1]
+    result, updates = theano.scan(fn=set_column,
+                                  outputs_info=T.zeros((out_height, n_positions, n_images)),
+                                  sequences=[T.arange(0, n_positions)],
+                                  non_sequences=[img])
+    return result[-1]
+
+
+def col2im(img, dims):
+    # dims = list(dims)
+    # for i in range(len(dims)):
+    #     if dims[i] == 1:
+    #         dims[i] = None
     n_images = img.shape[2]
-    strides = T.arange(0, img.shape[1])
-    trimmed = img[0:depth,strides,:]
+    trimmed = img[0:dims[2],:,:]
     rearranged = trimmed.dimshuffle((2, 1, 0))
-    shaped = T.reshape(rearranged, (n_images, dims[1], dims[0], depth))
+    shaped = T.reshape(rearranged, (n_images, dims[1], dims[0], dims[2]))
     return shaped.dimshuffle((2, 1, 3, 0))
+
+# Converts an image in column format with 1x1 kernel
+# to an image in column format with new kernel size
+def col2col(img, kernel_size):
+    depth = img.shape[0]
 
 
 if __name__ == '__main__':
     T_img = T.tensor4('img')
-    reshaped = im2col(T_img, 3)
+    reshaped = im2col(T_img, 2)
     flat_func = Tfunc([T_img], reshaped)
-    my_img = np.arange(32).reshape((4,4,2,1))
+    my_img = np.arange(16).reshape((4,4,1,1))
     np_reshaped = flat_func(my_img)
     print(my_img[:,:,0,0])
-    print(my_img[:,:,1,0])
+    # print(my_img[:,:,1,0])
     print(np_reshaped[:,:,0])
 
     flat_img = T.tensor3('flatimg')
-    shaped = col2im(flat_img, (2,2), 3, 2)
+    shaped = col2im(flat_img, (3,3,4))
     shape_func = Tfunc([flat_img], shaped)
     shaped = shape_func(np_reshaped)
     print(shaped[:,:,0,0])
-    print(shaped[:,:,1,0])
+    # print(shaped[:,:,1,0])
